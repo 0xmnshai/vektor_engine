51f50e37dd1629afe535a127df7f461894e72e48
Anshuman Tiwari <lazycodebaker@outlook.com>
2026-02-10 20:08:48 +0530

    scene hierachy fix

--- vektor_editor/include/panel/scene_hierachy.hpp
+++ vektor_editor/include/panel/scene_hierachy.hpp
@@ -1,6 +1,6 @@
 #pragma once
-
 #include <imgui.h>
+#include <memory>
 
 #include "vektor.hpp"
 
@@ -8,23 +8,22 @@ class SceneHierarchyPanel
 {
 public:
     SceneHierarchyPanel() = default;
-    SceneHierarchyPanel(const std::shared_ptr<vektor::world::scene::Scene> &context);
+    SceneHierarchyPanel(const std::shared_ptr<vektor::world::scene::Scene>& context);
 
-    void setContext(const std::shared_ptr<vektor::world::scene::Scene> &context);
+    void setContext(const std::shared_ptr<vektor::world::scene::Scene>& context);
     void onRender();
 
     vektor::world::ecs::entity_manager::Entity getSelectedEntity() const { return m_SelectionContext; }
-    void setSelectedEntity(vektor::world::ecs::entity_manager::Entity entity);
+    void setSelectedEntity(vektor::world::ecs::entity_manager::Entity entity) { m_SelectionContext = entity; }
 
 private:
     void drawEntityNode(vektor::world::ecs::entity_manager::Entity entity);
     void drawContextMenu();
-
-    void reparentEntity(vektor::world::ecs::entity_manager::Entity entity, vektor::world::ecs::entity_manager::Entity newParent);
+    void reparentEntity(vektor::world::ecs::entity_manager::Entity entity,
+                        vektor::world::ecs::entity_manager::Entity newParent);
 
 private:
     std::shared_ptr<vektor::world::scene::Scene> m_Context;
-    vektor::world::ecs::entity_manager::Entity m_SelectionContext;
-
-    char m_SearchBuffer[256] = {0};
+    vektor::world::ecs::entity_manager::Entity m_SelectionContext{};
+    char m_SearchBuffer[256] = {};
 };
--- vektor_editor/src/scene_hierachy.cpp
+++ vektor_editor/src/scene_hierachy.cpp
@@ -1,15 +1,16 @@
-
-
-#include <imgui_internal.h>
-
 #include "panel/scene_hierachy.hpp"
+#include <imgui_internal.h>
+#include <algorithm>
 
-SceneHierarchyPanel::SceneHierarchyPanel(const std::shared_ptr<vektor::world::scene::Scene> &context)
+using namespace vektor::world;
+using namespace vektor::world::ecs;
+
+SceneHierarchyPanel::SceneHierarchyPanel(const std::shared_ptr<scene::Scene> &context)
 {
     setContext(context);
 }
 
-void SceneHierarchyPanel::setContext(const std::shared_ptr<vektor::world::scene::Scene> &context)
+void SceneHierarchyPanel::setContext(const std::shared_ptr<scene::Scene> &context)
 {
     m_Context = context;
     m_SelectionContext = {};
@@ -19,208 +20,151 @@ void SceneHierarchyPanel::onRender()
 {
     ImGui::Begin("Scene Hierarchy");
 
-    if (m_Context)
+    if (!m_Context)
     {
-        ImGui::InputTextWithHint("##Search", "Search Entities...", m_SearchBuffer, sizeof(m_SearchBuffer));
-        ImGui::Separator();
+        ImGui::End();
+        return;
+    }
 
-        auto view = m_Context->getRegistry().view<vektor::world::ecs::component_storage::TagComponent>();
-        for (auto entity : view)
-        {
-            auto &transform = view.get<vektor::world::ecs::component_storage::TagComponent>(entity);
-            std::string tag = transform.getTag() + "-" + std::to_string((uint32_t)entity);
+    ImGui::InputTextWithHint("Search", "Search Entities...", m_SearchBuffer, sizeof(m_SearchBuffer));
+    // ImGui::InputTextWithHint("##Search", "Search Entities...", m_SearchBuffer, sizeof(m_SearchBuffer));
+    ImGui::Separator();
 
-            ImGui::Text("%s", tag.c_str());
+    auto &registry = m_Context->getRegistry();
+    auto view = registry.view<
+        component_storage::TransformComponent,
+        component_storage::TagComponent>();
 
-            drawEntityNode({entity, m_Context.get()});
-        }
+    view.each([this](auto entity, auto &transform, auto &tag)
+              {
+            if (transform.parent == 0) // root only
+                drawEntityNode({entity, m_Context.get()}); });
+
+    if (ImGui::BeginPopupContextWindow(nullptr, 1))
+    {
+        drawContextMenu();
+        ImGui::EndPopup();
     }
 
     ImGui::End();
 }
 
-void SceneHierarchyPanel::drawEntityNode(vektor::world::ecs::entity_manager::Entity entity)
+void SceneHierarchyPanel::drawEntityNode(entity_manager::Entity entity)
 {
+    auto &tag = entity.getComponent<component_storage::TagComponent>().getTag();
+    auto &transform = entity.getComponent<component_storage::TransformComponent>();
 
-    auto &tag = entity.getComponent<vektor::world::ecs::component_storage::TagComponent>();
-    std::string tagStr = tag.getTag() + "-" + std::to_string((uint32_t)entity);
+    // Search filter
+    if (strlen(m_SearchBuffer) > 0)
+    {
+        std::string tagLower = tag;
+        std::string search = m_SearchBuffer;
+        std::transform(tagLower.begin(), tagLower.end(), tagLower.begin(), ::tolower);
+        std::transform(search.begin(), search.end(), search.begin(), ::tolower);
+
+        if (tagLower.find(search) == std::string::npos)
+            return;
+    }
 
     ImGuiTreeNodeFlags flags =
         ((m_SelectionContext == entity) ? ImGuiTreeNodeFlags_Selected : 0) | ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_SpanAvailWidth;
 
-    ImGui::PushID((uint32_t)entity);
+    if (transform.children.empty())
+        flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
+    flags |= ImGuiTreeNodeFlags_OpenOnArrow;
 
-    bool opened = ImGui::TreeNodeEx("##entity", flags, "%s", tagStr.c_str());
+    bool opened = ImGui::TreeNodeEx((void *)(uint64_t)(uint32_t)entity, flags,
+                                    "%s-%u", tag.c_str(), (uint32_t)entity);
 
     if (ImGui::IsItemClicked())
         m_SelectionContext = entity;
 
-    if (opened)
-        ImGui::TreePop();
+    if (ImGui::BeginPopupContextItem("EntityContext")) //    if (ImGui::BeginPopupContextItem())
+    {
+        m_SelectionContext = entity;
 
-    ImGui::PopID();
+        if (ImGui::MenuItem("Create Child"))
+        {
+            auto child = m_Context->createEntity("Child");
+            reparentEntity(child, entity);
+        }
+
+        if (ImGui::MenuItem("Delete"))
+        {
+            m_Context->getRegistry().destroy((entt::entity)entity);
+            ImGui::EndPopup();
+            return;
+        }
+
+        ImGui::EndPopup();
+    }
+
+    if (ImGui::BeginDragDropSource())
+    {
+        uint32_t id = (uint32_t)entity;
+        ImGui::SetDragDropPayload("VEKTOR_ENTITY", &id, sizeof(uint32_t));
+        ImGui::Text("%s", tag.c_str());
+        ImGui::EndDragDropSource();
+    }
+
+    if (ImGui::BeginDragDropTarget())
+    {
+        if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("VEKTOR_ENTITY"))
+        {
+            uint32_t dropped = *(uint32_t *)payload->Data;
+            reparentEntity({(entt::entity)dropped, m_Context.get()}, entity);
+        }
+        ImGui::EndDragDropTarget();
+    }
+
+    if (opened && !transform.children.empty())
+    {
+        for (uint32_t child : transform.children)
+            drawEntityNode({(entt::entity)child, m_Context.get()});
+
+        ImGui::TreePop();
+    }
 }
 
-// #include <imgui_internal.h>
+void SceneHierarchyPanel::drawContextMenu()
+{
+    if (ImGui::MenuItem("Create Empty"))
+        m_Context->createEntity("Empty");
 
-// #include "panel/scene_hierachy.hpp"
+    if (ImGui::MenuItem("Create Camera"))
+    {
+        auto e = m_Context->createEntity("Camera");
+        e.addComponent<component_storage::CameraComponent>();
+    }
+}
 
-// SceneHierarchyPanel::SceneHierarchyPanel(const std::shared_ptr<vektor::world::scene::Scene> &context)
-// {
-//     setContext(context);
-// }
+void SceneHierarchyPanel::reparentEntity(entity_manager::Entity entity,
+                                         entity_manager::Entity newParent)
+{
+    auto &transform = entity.getComponent<component_storage::TransformComponent>();
 
-// void SceneHierarchyPanel::setContext(const std::shared_ptr<vektor::world::scene::Scene> &context)
-// {
-//     m_Context = context;
-//     m_SelectionContext = {};
-// }
+    if (transform.parent != 0)
+    {
+        auto &oldParent =
+            m_Context->getRegistry().get<component_storage::TransformComponent>(
+                (entt::entity)transform.parent);
 
-// void SceneHierarchyPanel::onRender()
-// {
-//     ImGui::Begin("Scene Hierarchy");
+        auto &children = oldParent.children;
+        children.erase(std::remove(children.begin(), children.end(), (uint32_t)entity),
+                       children.end());
+    }
 
-//     if (m_Context)
-//     {
-//         ImGui::InputTextWithHint("##Search", "Search Entities...", m_SearchBuffer, sizeof(m_SearchBuffer));
-//         ImGui::Separator();
+    if (newParent)
+    {
+        transform.parent = (uint32_t)newParent;
+        auto &newParentTransform =
+            newParent.getComponent<component_storage::TransformComponent>();
+        newParentTransform.children.push_back((uint32_t)entity);
+    }
+    else
+    {
+        transform.parent = 0;
+    }
 
-//         auto view = m_Context->getRegistry().view<vektor::world::ecs::component_storage::TransformComponent>();
-//         for (auto entityID : view)
-//         {
-//             auto &transform = view.get<vektor::world::ecs::component_storage::TransformComponent>(entityID);
-
-//             if (transform.parent == 0)
-//             {
-//                 drawEntityNode({entityID, m_Context.get()});
-//             }
-//         }
-
-//         if (ImGui::BeginPopupContextWindow(0, 1))
-//         {
-//             drawContextMenu();
-//             ImGui::EndPopup();
-//         }
-//     }
-
-//     ImGui::End();
-// }
-
-// void SceneHierarchyPanel::drawEntityNode(vektor::world::ecs::entity_manager::Entity entity)
-// {
-//     auto &tag = entity.getComponent<vektor::world::ecs::component_storage::TagComponent>().tag;
-//     auto &transform = entity.getComponent<vektor::world::ecs::component_storage::TransformComponent>();
-
-//     ImGui::PushID((int)(uint32_t)entity);
-
-//     if (strlen(m_SearchBuffer) > 0)
-//     {
-//         std::string tagLower = tag;
-//         std::transform(tagLower.begin(), tagLower.end(), tagLower.begin(), ::tolower);
-//         std::string searchLower = m_SearchBuffer;
-//         std::transform(searchLower.begin(), searchLower.end(), searchLower.begin(), ::tolower);
-//         if (tagLower.find(searchLower) == std::string::npos)
-//             return;
-//     }
-
-//     ImGuiTreeNodeFlags flags = ((m_SelectionContext == entity) ? ImGuiTreeNodeFlags_Selected : 0);
-//     flags |= ImGuiTreeNodeFlags_OpenOnArrow;
-//     flags |= ImGuiTreeNodeFlags_SpanAvailWidth;
-
-//     if (transform.children.empty())
-//         flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
-
-//     // bool opened = ImGui::TreeNodeEx((void *)(uint64_t)(uint32_t)entity, flags, tag.c_str());
-
-//     bool opened = ImGui::TreeNodeEx(tag.c_str(), flags);
-
-//     if (ImGui::IsItemClicked())
-//     {
-//         m_SelectionContext = entity;
-//     }
-
-//     if (ImGui::BeginPopupContextItem())
-//     {
-//         m_SelectionContext = entity;
-//         if (ImGui::MenuItem("Create Child Entity"))
-//         {
-//             auto child = m_Context->createEntity("Child Entity");
-//             reparentEntity(child, entity);
-//         }
-//         ImGui::Separator();
-//         if (ImGui::MenuItem("Delete Entity"))
-//         {
-//             if (m_SelectionContext == entity)
-//                 m_SelectionContext = {};
-//             m_Context->getRegistry().destroy(entity);
-//             ImGui::EndPopup();
-//         }
-//         ImGui::EndPopup();
-//     }
-
-//     if (ImGui::BeginDragDropSource())
-//     {
-//         uint32_t entityID = (uint32_t)entity;
-//         ImGui::SetDragDropPayload("PROJECT_EXPLORER_ENTITY", &entityID, sizeof(uint32_t));
-//         ImGui::Text("%s", tag.c_str());
-//         ImGui::EndDragDropSource();
-//     }
-
-//     if (ImGui::BeginDragDropTarget())
-//     {
-//         if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("PROJECT_EXPLORER_ENTITY"))
-//         {
-//             uint32_t droppedEntityID = *(const uint32_t *)payload->Data;
-//             reparentEntity({(entt::entity)droppedEntityID, m_Context.get()}, entity);
-//         }
-//         ImGui::EndDragDropTarget();
-//     }
-
-//     if (opened && !transform.children.empty())
-//     {
-//         for (auto childID : transform.children)
-//         {
-//             drawEntityNode({(entt::entity)childID, m_Context.get()});
-//         }
-//         ImGui::TreePop();
-//     }
-
-//     ImGui::PopID();
-// }
-
-// void SceneHierarchyPanel::drawContextMenu()
-// {
-//     if (ImGui::MenuItem("Create Empty Entity"))
-//         m_Context->createEntity("Empty Entity");
-
-//     if (ImGui::MenuItem("Create Camera"))
-//     {
-//         auto e = m_Context->createEntity("Camera");
-//         e.addComponent<vektor::world::ecs::component_storage::CameraComponent>();
-//     }
-// }
-
-// void SceneHierarchyPanel::reparentEntity(vektor::world::ecs::entity_manager::Entity entity, vektor::world::ecs::entity_manager::Entity newParent)
-// {
-//     auto &transform = entity.getComponent<vektor::world::ecs::component_storage::TransformComponent>();
-
-//     if (transform.parent != 0)
-//     {
-//         auto &oldParentTransform = m_Context->getRegistry().get<vektor::world::ecs::component_storage::TransformComponent>((entt::entity)transform.parent);
-//         auto &children = oldParentTransform.children;
-//         children.erase(std::remove(children.begin(), children.end(), (uint32_t)entity), children.end());
-//     }
-
-//     if (newParent)
-//     {
-//         transform.parent = (uint32_t)newParent;
-//         auto &newParentTransform = newParent.getComponent<vektor::world::ecs::component_storage::TransformComponent>();
-//         newParentTransform.children.push_back((uint32_t)entity);
-//     }
-//     else
-//     {
-//         transform.parent = 0;
-//     }
-
-//     transform.markDirty();
-// }
+    transform.markDirty();
+}
